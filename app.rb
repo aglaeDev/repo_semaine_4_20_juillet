d1ce9d6ad1d51d63ecd1b00e659c6919bef7ca82
aglaeDev <a.gourdet.dev@gmail.com>
2023-07-20 17:52:18 +0200

    avnat dernier commit

--- app.rb
+++ app.rb
@@ -4,39 +4,78 @@ require_relative 'lib/player/player.rb'
 
 puts "Deux joueurs s'affrontent. Ils doivent remplir chacun à leur tour une case de la grille avec le symbole qui leur est attribué : O ou X. Le gagnant est celui qui arrive à aligner trois symboles identiques, horizontalement, verticalement ou en diagonale. 
 Il est coutume de laisser le joueur jouant X effectuer le premier coup de la partie."
-class Game
-    #TO DO : la classe a plusieurs attr_accessor: le current_player (égal à un objet Player), le status (en cours, nul ou un objet Player s'il gagne), le Board et un array contenant les 2 joueurs.
-  
-    def initialize
-      #TO DO : créé 2 joueurs, créé un board, met le status à "on going", défini un current_player
-    end
-  
-    def turn
-      #TO DO : méthode faisant appelle aux méthodes des autres classes (notamment à l'instance de Board). Elle affiche le plateau, demande au joueur ce qu'il joue, vérifie si un joueur a gagné, passe au joueur suivant si la partie n'est pas finie.
-    end
-  
-    def new_round
-      # TO DO : relance une partie en initialisant un nouveau board mais en gardant les mêmes joueurs.
-    end
-  
-    def game_end
-      # TO DO : permet l'affichage de fin de partie quand un vainqueur est détecté ou si il y a match nul
-    end    
-  
-end
 
 class Show
 
     def show_board(board)
-      #TO DO : affiche sur le terminal l'objet de classe Board en entrée. S'active avec un Show.new.show_board(instance_de_Board)
+        puts "\n"
+        puts " 1 | 2 | 3"
+        puts "---+---+---"
+        puts " 4 | 5 | 6"
+        puts "---+---+---"
+        puts " 7 | 8 | 9 "
+        puts "\n"
     end
   
 end
+
+class Game
+    #TO DO : la classe a plusieurs attr_accessor: le current_player (égal à un objet Player), le status (en cours, nul ou un objet Player s'il gagne), le Board et un array contenant les 2 joueurs.
   
-class Application
-    def perform
-      # TO DO : méthode qui initialise le jeu puis contient des boucles while pour faire tourner le jeu tant que la partie n'est pas terminée.
-      # 
+    def initialize
+        def initialize
+            puts "Joueur 1, entrez votre nom :"
+            name1 = gets.chomp
+            @player1 = Player.new(name1, "X")
+            
+            puts "Joueur 2, entrez votre nom :"
+            name2 = gets.chomp
+            @player2 = Player.new(name2, "O")
+            
+            @board = Board.new
+            @current_player = @player1
+          end
     end
   
+    def turn
+        @board.display_board
+        @board.play_turn(@current_player)
+    
+        if @board.victory?
+          @board.display_board
+          puts "#{@current_player.name} a gagné ! Félicitations !"
+        else
+          switch_player
+        end
+    end
+
+    def switch_player
+        @current_player = (@current_player == @player1) ? @player2 : @player1
+      end
+  
+    def new_round
+        @board = Board.new
+        @current_player = @player1
+        play
+    end
+  
+    def game_end
+      @board = Board.new
+        @current_player = @player1
+        play
+    end   
+
+    def play
+        turn until @board.victory?
+        puts "Partie terminée ! Voulez-vous rejouer ? (Oui/Non)"
+        response = gets.chomp.downcase
+
+        if response == "oui"
+        new_round
+        else
+        puts "Merci d'avoir joué ! À bientôt !"
+        end 
+    end
 end
+
+Game.new.play
